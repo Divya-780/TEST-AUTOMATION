757cf18617c1f7f9b2becf4432d69d29ef85bde0
Divya-780 <divya@applines.com>
2022-08-29 17:51:31 +0530

    Changes for favorites

--- Projects/Cypress/Project1/cypress/integration/favorites/favorites-page.spec.js
+++ Projects/Cypress/Project1/cypress/integration/favorites/favorites-page.spec.js
@@ -1,21 +1,16 @@
-//Favorite speeches are displayed in favorites page.
-describe('Favorite speeches are displayed in favorites page', function () {
-    it(' Verify that all favorite speeches are displayed in favorites page', function () {
-     //login as a valid user.
-     let url = Cypress.config().baseUrl; //accesing baseUrl
-     cy.visit(url);
-     cy.contains('Sign in').click();     
-     cy.get('#email').type('sowbhagya3696+4@gmail.com');
-     cy.get('#password').type('Sathwik@1719');
-     cy.get('form').submit();
-     //navigate to Activity page
-     cy.contains('Activity');
-     cy.get('.fa-lg').first().click();
-     //click on Favorites link
-     cy.get('[routerlink="/activity/favorites"]').click();
-     //verify that above speech is displayed
-     cy.contains('Favorites');
-    })
-   })
- 
-
+//User shall be able to redirect to favorites page.
+describe("User shall be able to redirect to favorites page", function() {
+    it(" Verify that user should redirect to favorites page", function() {
+        let url = Cypress.config().baseUrl;
+        cy.visit(url);
+        cy.contains("Sign in").click();
+        cy.get("#email").type("sowbhagya3696+4@gmail.com");
+        cy.get("#password").type("Sathwik@1719");
+        cy.get("form").submit();
+        //navigate to Favorites page
+        cy.get('[routerlink="/activity/favorites"]').click();
+        //verify that favorites page is opened or not
+        cy.contains("Favorites").should("exist");
+        // cy.get(".list-group-item").its("length").should("eq", 2);
+    });
+});
