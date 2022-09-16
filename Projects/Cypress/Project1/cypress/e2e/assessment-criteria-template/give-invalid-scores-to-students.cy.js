5f1cf448080367dbc9397ffe2f1c197718933d12
tejasri12345 <89897456+tejasri12345@users.noreply.github.com>
2022-09-16 11:13:36 +0530

    Updated

--- Projects/Cypress/Project1/cypress/e2e/assessment-criteria-template/give-invalid-scores-to-students.cy.js
+++ Projects/Cypress/Project1/cypress/e2e/assessment-criteria-template/give-invalid-scores-to-students.cy.js
@@ -1,23 +1,23 @@
 //Coach should not allow to give scores which are not matched to the assigned weight
-describe("Coach giving invalid scores irrespective of the weight", function () {
-  it("Coach giving invalid scores.", function () {
-    // visit signin url.
-    let url = Cypress.config().baseUrl;
-    cy.visit(url);
-    cy.contains("Sign in").click();
-    cy.get("#email").type("sowbhagya3696+4@gmail.com");
-    cy.get("#password").type("Sathwik@1719");
-    cy.get("form").submit();
-    cy.contains("Student Activity").click();
-    cy.wait(4000);
-    cy.get(".p-shadow-4").first().click({ force: true });
-    cy.wait(6000);
-    cy.get(".fa-pencil").eq(0).click();
-    cy.get(".p-inputtext").clear().type("100");
-    cy.get(".btn").click();
-    // Validation
-    cy.contains("Score should not be greater than Weight").should("exist");
-    //--TEAR DOWN --//
-    cy.get(".fa-sign-out-alt").click({ force: true });
-  });
+describe("Coach giving invalid scores irrespective of the weight", function() {
+    it("Coach giving invalid scores.", function() {
+        // visit signin url.
+        let url = Cypress.config().baseUrl;
+        cy.visit(url);
+        cy.contains("Sign in").click();
+        cy.get("#email").type("sowbhagya3696+4@gmail.com");
+        cy.get("#password").type("Sathwik@1719");
+        cy.get("form").submit();
+        cy.contains("Student Activity").click();
+        cy.wait(4000);
+        cy.get(".p-shadow-4").eq(2).click({ force: true });
+        cy.wait(6000);
+        cy.get(".fa-pencil").eq(1).click();
+        cy.get(".p-inputtext").clear().type("100");
+        cy.get(".btn").click();
+        // Validation
+        cy.contains("Score should not be greater than Weight").should("exist");
+        //--TEAR DOWN --//
+        cy.get(".fa-sign-out-alt").click({ force: true });
+    });
 });
--- Projects/Cypress/Project1/cypress/e2e/subscription/receipt-view.cy.js
+++ Projects/Cypress/Project1/cypress/e2e/subscription/receipt-view.cy.js
@@ -1,22 +1,22 @@
 //User shall receive the receipt for a successful payment.
-describe("User will receive the receipt for a successful payment. ", function () {
-  it("User can check the receipt after the successful transaction ", function () {
-    // Login with valid credentials
-    let url = Cypress.config().baseUrl;
-    cy.visit(url);
-    cy.contains("Sign in").click(); //Login with valid credentials
-    cy.get("#email").type("applinetester+1@gmail.com");
-    cy.get("#password").type("Sathwik@1719");
-    cy.get("form").submit();
-    cy.wait(1000);
-    //redirecting to subscription page
-    cy.get('[routerlink="/user/groups"]').click({ force: true });
-    cy.get(".nav-item").invoke("show");
-    cy.get('[routerlink="/user/subscriptions"]').click({ force: true });
-    cy.contains("Invoice #").click();
-    //-- VALIDATION --//
-    //Observe that user is taken to payment receipt page
-    //--TEAR DOWN --//
-    cy.get(".fa-sign-out-alt").click({ force: true });
-  });
+describe("User will receive the receipt for a successful payment. ", function() {
+    it("User can check the receipt after the successful transaction ", function() {
+        // Login with valid credentials
+        let url = Cypress.config().baseUrl;
+        cy.visit(url);
+        cy.contains("Sign in").click(); //Login with valid credentials
+        cy.get("#email").type("applinetester+1@gmail.com");
+        cy.get("#password").type("Sathwik@1719");
+        cy.get("form").submit();
+        cy.wait(1000);
+        //redirecting to subscription page
+        cy.get('[routerlink="/user/groups"]').click({ force: true });
+        cy.get(".nav-item").invoke("show");
+        cy.get('[routerlink="/user/subscriptions"]').click({ force: true });
+        cy.contains("Invoice #");
+        //-- VALIDATION --//
+        //Observe that user is taken to payment receipt page
+        //--TEAR DOWN --//
+        cy.get(".fa-sign-out-alt").click({ force: true });
+    });
 });
