e98a68fcdcb70adc0d2214e6badacd2f4a81ebfe
Gayathri547 <89897862+Gayathri547@users.noreply.github.com>
2022-09-22 14:55:54 +0530

    API for feedback point

--- Projects/Cypress/SP-API/cypress/e2e/core/feedback/delete_feedback_point_by_id.cy.js
+++ Projects/Cypress/SP-API/cypress/e2e/core/feedback/delete_feedback_point_by_id.cy.js
@@ -7,10 +7,11 @@ describe('Delete feedback point API Test', function () {
             headers: {
                 "Authorization": "Token 4872aae72371e9a72f50a13aac2f4d92362ca57c"
             },
+            failOnStatusCode: false
         }).then(
             (response) => {
-                // Verify response - status, header values
-                expect(response.status).to.eq(200)
+                // For already deleted feedback point, the status should be 404
+                expect(response.status).to.eq(404)
 
             }
         )
--- Projects/Cypress/SP-API/cypress/e2e/core/feedback/put_feedback_by_point_by_id.cy.js
+++ Projects/Cypress/SP-API/cypress/e2e/core/feedback/put_feedback_by_point_by_id.cy.js
@@ -2,16 +2,16 @@ describe('Update feedback point API', function () {
     it('Verifies that API used to update the keypoints of the particular feedback', function () {
         cy.request({
             method: 'PUT',
-            url: 'https://dev02.speechpundit.com/api/core/feedback/point/474/',
+            url: 'https://dev02.speechpundit.com/api/core/feedback/point/495/',
             headers: {
               Authorization: 'Token 4872aae72371e9a72f50a13aac2f4d92362ca57c',
             },
             body:{
-                "id": 474,
-                "description": "Project your voice to the front of the room.kjljkl",
-                "time": 11,
-                "createdAt": "2022-09-20T07:27:49.397792Z",
-                "updatedAt": "2022-09-20T07:27:49.397840Z",
+                "id": 495,
+                "description": "Make eye contact. update-update",
+                "time": 10,
+                "createdAt": "2022-09-22T09:15:02.131924Z",
+                "updatedAt": "2022-09-22T09:15:02.131980Z",
                 "isGrow": true
             },
 
